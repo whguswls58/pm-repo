c702b950d1a0ca4d6f838b5ca06f03bf48eaddf1
jooelrm <jooelrm@outlook.com>
2020-04-22 17:17:41 +0900

    fix: p -> f

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,8 @@
 for i in range(1,100+1):
     if i%15 ==0:
-        print("pizz&buzz")
+        print("fizz&buzz")
     elif i%3 ==0:
-        print("pizz")
+        print("fizz")
     elif i%5 ==0:
         print("buzz")
 
