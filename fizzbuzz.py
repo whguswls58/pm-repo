4b37ce0a5679cf6abc5bf0db39ab114090a1cc27
peterdy-shin <petshin0220@gmail.com>
2020-04-22 17:32:24 +0900

    feat: if clause edited

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,6 @@
 for i in range(1,100+1):
-    if i%15 ==0:
-        print("pizz&buzz")
-    elif i%3 ==0:
-        print("pizz")
-    elif i%5 ==0:
-        print("buzz")
-
+    if i%3==0 or i%5==0:
+	print("fizz"*(i%3==0)+"buzz"*(i%5==0))
     else:
         print("i")
 
