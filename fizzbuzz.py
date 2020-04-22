b6cdfc1fe153e54718397536a4a102bc28ffad5d
micro94 <jhs941222@naver.com>
2020-04-22 17:06:25 +0900

    print fizz&buzz
    if i%15 ==0 "fizz&buzz"
    elif i%3 ==0 "fizz"
    else i%5 ==0 "buzz"

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,11 @@
+for i in range(1,100+1):
+    if i%15 ==0:
+        print("pizz&buzz")
+    elif i%3 ==0:
+        print("pizz")
+    elif i%5 ==0:
+        print("buzz")
+
+    else:
+        print("i")
 
